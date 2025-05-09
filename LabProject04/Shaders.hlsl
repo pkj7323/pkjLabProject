966e128fcfdf1804106a8587ef7a3c9bbe9586e8
leopark <pkj7323@naver.com>
2025-05-09 14:58:11 +0900

    • 화면 중심에서 반지름 0.25 이내의 영역은 파란색으로 채워지고, 나머지 영역은 검은색으로 렌더링됩니다. • 화면 비율이 조정되므로 원이 찌그러지지 않고 올바르게 표시됩니다.

--- LabProject04/Shaders.hlsl
+++ LabProject04/Shaders.hlsl
@@ -22,10 +22,10 @@ float4 VSMain(uint nVertexID : SV_VertexID) : SV_POSITION
 float4 PSMain(float4 input : SV_POSITION) : SV_TARGET
 {
     float4 cColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
-    //cColor.rgb = distance(float2(0.0f, 0.0f), input.xy / float2(FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT));
-    //cColor.rgb = length(input.xy / float2(FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT));
-    //cColor.rgb = distance(float2(0.0f, 0.0f), (input.xy / float2(FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT)-0.5f)); // (0, 1) : (-0.5, 0.5)
-    //cColor.rgb = length(input.xy / float2(FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT) - 0.5f);
-    cColor.rgb = distance(float2(0.5f, 0.5f), input.xy / float2(FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT));
+    float2 f2NDC = float2(input.x / FRAME_BUFFER_WIDTH, input.y / FRAME_BUFFER_HEIGHT) - 0.5f; // (0, 1) : (-0.5, 0.5)
+    //f2NDC.x *= (FRAME_BUFFER_WIDTH / FRAME_BUFFER_HEIGHT);
+	cColor.b = step(length(f2NDC), 0.25f); //step(x, y) = (x <= y) ? 1 : 0
+    //위아래 동일한 코드 스탭을 쓰면 좀더 간결하고 최적화 된 코드 가능
+    //cColor.b = (length(f2NDC) <= 0.25f) ? 1.0f : 0.0f; //step(x, y) = (x <= y) ? 1 : 0
     return (cColor);
 }
