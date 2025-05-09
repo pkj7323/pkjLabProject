bb594549cfef3974266d3d417a8c786765daa2ee
leopark <pkj7323@naver.com>
2025-05-09 15:17:43 +0900

    가운데 하얀색 원으로 번짐 효과

--- LabProject04/Shaders.hlsl
+++ LabProject04/Shaders.hlsl
@@ -22,10 +22,10 @@ float4 VSMain(uint nVertexID : SV_VertexID) : SV_POSITION
 float4 PSMain(float4 input : SV_POSITION) : SV_TARGET
 {
     float4 cColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
-    float2 f2NDC = float2(input.x / FRAME_BUFFER_WIDTH, input.y / FRAME_BUFFER_HEIGHT) - 0.5f; // (0, 1) : (-0.5, 0.5)
-    //f2NDC.x *= (FRAME_BUFFER_WIDTH / FRAME_BUFFER_HEIGHT);
-	cColor.b = step(length(f2NDC), 0.25f); //step(x, y) = (x <= y) ? 1 : 0
-    //위아래 동일한 코드 스탭을 쓰면 좀더 간결하고 최적화 된 코드 가능
-    //cColor.b = (length(f2NDC) <= 0.25f) ? 1.0f : 0.0f; //step(x, y) = (x <= y) ? 1 : 0
+    float2 f2NDC = float2(input.x / FRAME_BUFFER_WIDTH, input.y / FRAME_BUFFER_HEIGHT) - 0.5f;
+    f2NDC.x *= (FRAME_BUFFER_WIDTH / FRAME_BUFFER_HEIGHT);
+    float fLength = length(f2NDC);
+    float fMin = 0.3f, fMax = 0.2f;
+    cColor.rgb = smoothstep(fMin, fMax, fLength);
     return (cColor);
 }
